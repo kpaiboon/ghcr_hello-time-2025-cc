1659d2a9fd9a6677af28b600594fef5978abc903
PaiboonKupthanakorn <143982351+PaiboonKupthanakorn@users.noreply.github.com>
2025-04-01 17:16:31 +0700

    Update app.pl

--- app.pl
+++ app.pl
@@ -5,12 +5,12 @@ use Time::Piece;
 use Mojo::Util qw(trim);
 
 my $greeting = trim( $ENV{GREETING_TEXT} || "Hello World" );
-my $port = $ENV{PORT} || 6000; # Default to port 6000
+my $port = $ENV{PORT} || 6000;
 
 get '/' => sub {
   my $self = shift;
   my $now = gmtime;
-  $self->render_text("$greeting and Time GMT: " . $now->strftime("%Y-%m-%d %H:%M:%S"));
+  $self->render(text => "$greeting and Time GMT: " . $now->strftime("%Y-%m-%d %H:%M:%S"));
 };
 
-app->start;
+app->start('daemon', '-l', "http://*:$port");
